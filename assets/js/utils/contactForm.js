09606ce358856664a56613cd23d7647b81ccb9dc
Anthony.M <marcellin.anthony77@gmail.com>
2022-09-20 18:35:34 +0200

    Remises en forme multiples d'éléments HTML

--- assets/js/utils/contactForm.js
+++ assets/js/utils/contactForm.js
@@ -2,13 +2,14 @@ function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
     modal.setAttribute('aria-hidden', 'false');
+    mainSection.setAttribute('aria-hidden', 'true');
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
     modal.setAttribute('aria-hidden', 'true');
-
+    mainSection.setAttribute('aria-hidden', 'false');
 }
 
 function createModalContact(namePhotographer) {
